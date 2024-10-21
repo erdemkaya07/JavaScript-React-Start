3d7a7b62b831cd130c6ee461e24b49857ac5c760
Erdem Kaya <kerdem@live.com>
2024-10-21 12:52:01 +0200

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,36 @@ import React, { useState } from 'react'
 
 function App() {
 
-  /* let counter = 0; */
-  const [counter, setCounter] = useState(0)
-  /* const handleClick = () => {
-    /* counter++ */
-    /* setCounter(counter + 1) */
-   /* setCounter(prev => prev +1)
-  } */
-  /* console.log(counter)
- */
+const [name, setName] = useState('Erdem')
+const [showName, setShowName] = useState(false)
 
+const [isActive, setIsActive] = useState(false)
 
-  const handleClick = (amount) => {
-    setCounter(prev => {
-      return prev + amount
-    })
-  }
-
-  const [firstName, setFirstName] = useState('erdem')
-  const [lastName, setlastName] = useState('kaya')
-
-
-  /* const [fullName, setfullName] = useState(firstName + ' ' + lastName) */
-
-  const fullName = firstName + ' ' + lastName;
+const toggleName = () => {
+  setShowName(value => !value)
+}
 
   return (
     <div className='container'>
-      <h1>{ counter }</h1>
-      {/* <button onClick={handleClick}>Increment</button> */}
-      <button onClick={() => handleClick(1)}>Increment</button>
-      <button onClick={() => handleClick(10)}>Increment</button>
-      <button onClick={() => handleClick(100)}>Increment</button>
+     <h1>Conditional Rendering</h1>
 
-      <div>
-      </div>
-        <p>{firstName} {lastName}</p>
-        <p>{fullName}</p>
-        <button onClick={() => {setlastName('svensson')}}>ChangeName</button>
+    {/*  { showName ? <p>{name}</p> : <p>Inget namn</p> } */}
+
+    {/*  { showName && <p>{name}</p> } */}
+
+     {/* { name && <p>{name}</p>} */}
+
+     <button onClick={toggleName}>{showName ? 'Hide Name' : 'Show Name'}</button>
+
+    {showName && <p>Name: {name}</p> }
+
+    <button onClick={() => {
+      setIsActive(prev => !prev)
+    }}>Click Name</button>
+
+    <div className={`square ${isActive ? 'active' : ''}`}>
+
+    </div>
     </div>
   )
 }
