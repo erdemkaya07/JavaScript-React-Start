e2fc545443233c867568c40ef839a04c4dc3b0e2
Erdem Kaya <kerdem@live.com>
2024-10-21 10:00:48 +0200

    React components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+
+function Footer() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <SearchForm />
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Hero() {
+  return (
+  <section>
+    <div className="container py-5">
+      <h1>Köpt grejer av oss!</h1>
+      <button className='btn btn-primary'>Boka tid nu!</button>
+    </div>
+  </section>
+  )
+}
+
+export default Hero
